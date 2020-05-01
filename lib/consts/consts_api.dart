154dc8b9b38ec357a1f9144d4ef4973cc862d902
Victor <vpma_amorim@hotmail.com>
2020-05-01 11:48:58 -0300

    Added consts API and APP

---
+++ lib/consts/consts_api.dart
@@ -0,0 +1,4 @@
+class ConstsApi {
+  static final urlPokeAPI =
+      'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+}
---
+++ lib/consts/consts_app.dart
@@ -0,0 +1,4 @@
+class ConstsApp {
+  static final String whitePokeball = 'assets/images/pokeball.png';
+  static final String blackPokeball = 'assets/images/pokeball_dark.png';
+}
