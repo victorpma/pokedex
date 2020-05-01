80577e54228f5fa8e6a6c1d62f43ac7b9b6487a2
Victor <vpma_amorim@hotmail.com>
2020-05-01 18:18:42 -0300

    Added consts

--- lib/consts/consts_api.dart
+++ lib/consts/consts_api.dart
@@ -1,4 +1,8 @@
 class ConstsApi {
-  static final urlPokeAPI =
+  static final String urlPokeAPI =
       'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+
+  // use sprintf to substitute %s by number pokemon
+  static final String urlPokeImage =
+      'http://www.serebii.net/pokemongo/pokemon/%s.png';
 }
