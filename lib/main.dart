efa432ccb987dfd6ddc1dd9ff2d93bdf0a049abe
Victor <vpma_amorim@hotmail.com>
2020-05-02 22:25:42 -0300

    Added Dependency Injection with GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pokedex/controllers/home_controller.dart';
+import 'package:pokedex/controllers/pokemon_detail_controller.dart';
 import 'views/home/home_view.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<HomeController>(HomeController(), signalsReady: true);
+  getIt.registerSingleton<PokemonDetailController>(PokemonDetailController(),
+      signalsReady: true);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
