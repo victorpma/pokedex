a97c16294a281f3f4831b40965a4e8c79fb0b5d4
Victor <vpma_amorim@hotmail.com>
2020-05-02 22:28:43 -0300

    Refactoring HomeView

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:get_it/get_it.dart';
 import 'package:pokedex/consts/consts_app.dart';
 import 'package:pokedex/controllers/home_controller.dart';
 import 'package:pokedex/views/home/widgets/appbar_widget.dart';
-import 'package:pokedex/views/home/widgets/pokemon_item.dart';
+import 'package:pokedex/views/home/widgets/gridview_widget.dart';
 
 class HomeView extends StatefulWidget {
+  final int index;
+
+  const HomeView({Key key, this.index}) : super(key: key);
+
   @override
   _HomeViewState createState() => _HomeViewState();
 }
 
-HomeController homeController;
-
 class _HomeViewState extends State<HomeView> {
+  HomeController homeController;
+
   @override
   void initState() {
     super.initState();
-    homeController = HomeController();
+
+    homeController = GetIt.instance<HomeController>();
     homeController.fetchPokemons();
   }
 
@@ -41,43 +45,7 @@ class _HomeViewState extends State<HomeView> {
           Column(children: <Widget>[
             SizedBox(height: heightAppBar),
             AppbarWidget(),
-            Expanded(
-              child: Container(child: Observer(
-                builder: (_) {
-                  return homeController.pokeApi == null
-                      ? Center(child: CircularProgressIndicator())
-                      : AnimationLimiter(
-                          child: GridView.builder(
-                              itemCount: homeController.pokeApi.pokemons.length,
-                              padding: EdgeInsets.all(12),
-                              addAutomaticKeepAlives: true,
-                              physics: BouncingScrollPhysics(),
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 2),
-                              itemBuilder: (context, index) {
-                                var pokemon =
-                                    homeController.pokeApi.pokemons[index];
-
-                                return AnimationConfiguration.staggeredGrid(
-                                    position: index,
-                                    duration: const Duration(milliseconds: 375),
-                                    columnCount: 2,
-                                    child: ScaleAnimation(
-                                      child: GestureDetector(
-                                        child: PokemonItem(
-                                            index: index,
-                                            name: pokemon.name,
-                                            number: pokemon.num,
-                                            types: pokemon.type),
-                                        onTap: () {},
-                                      ),
-                                    ));
-                              }),
-                        );
-                },
-              )),
-            )
+            GridViewWidget(),
           ])
         ],
       ),
---
+++ lib/views/home/widgets/gridview_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pokedex/controllers/home_controller.dart';
+import 'package:pokedex/controllers/pokemon_detail_controller.dart';
+import 'package:pokedex/views/home/widgets/pokemon_item.dart';
+import 'package:pokedex/views/pokemon_detail/pokemon_detail_view.dart';
+
+class GridViewWidget extends StatelessWidget {
+  var homeController = GetIt.instance<HomeController>();
+  var pokemonDetailController = GetIt.instance<PokemonDetailController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(child: Observer(
+        builder: (_) {
+          return homeController.pokeApi == null
+              ? Center(child: CircularProgressIndicator())
+              : AnimationLimiter(
+                  child: GridView.builder(
+                      itemCount: homeController.pokeApi.pokemons.length,
+                      padding: EdgeInsets.all(12),
+                      addAutomaticKeepAlives: true,
+                      physics: BouncingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        var pokemon = homeController.pokeApi.pokemons[index];
+
+                        return AnimationConfiguration.staggeredGrid(
+                            position: index,
+                            duration: const Duration(milliseconds: 375),
+                            columnCount: 2,
+                            child: ScaleAnimation(
+                              child: GestureDetector(
+                                child: PokemonItem(
+                                    index: index,
+                                    name: pokemon.name,
+                                    number: pokemon.num,
+                                    types: pokemon.type),
+                                onTap: () {
+                                  pokemonDetailController.setCurrentPokemon(
+                                      homeController.pokeApi.pokemons[index]);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              PokemonDetailView(index: index)));
+                                },
+                              ),
+                            ));
+                      }),
+                );
+        },
+      )),
+    );
+  }
+}
